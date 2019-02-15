d68a57866e60b0c36ea0124d8f39c7933fd90ff7
Xavier Sala <utrescu@gmail.com>
2019-02-15 16:23:57 +0100

    extreure cadenes i  missatges d'error

--- Program.cs
+++ Program.cs
@@ -8,10 +8,16 @@ namespace AligaAuth
     [HelpOption("-?")]
     class Program
     {
+        // Missatges
         private const string UsuariError = "Credencials invàlides\n";
         private const string UsuariCorrecte = " OK\n";
+
+        // Constants LDAP que s'hauríen de passar com a configuració
+        private const string LdapServer = "localhost";
         private const string LdapCNUser = "cn=readonly,dc=aliga,dc=cat";
         private const string LdapCNPassword = "passwr0rd!";
+        private const string LdapUsers = "ou=People,dc=aliga,dc=cat";
+
 
         static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
 
@@ -23,8 +29,15 @@ namespace AligaAuth
 
         private void OnExecute()
         {
-            var username = Username ?? "helena";
+            var username = Username ?? "";
             var password = Password ?? "";
+
+            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                System.Console.WriteLine("\nAfegeix l'usuari i la contrasenya darrere de la comanda\n");
+                System.Console.WriteLine(" ex. dotnet run Usuari Password\n");
+                return;
+            }
 
             username = username.ToLower();
             System.Console.WriteLine($"... Provant usuari: {username} i contrasenya {password}\n");
@@ -34,13 +47,13 @@ namespace AligaAuth
                 try
                 {
                     // connect
-                    cn.Connect("localhost", 389);
-                    // ... Bind CN
+                    cn.Connect(LdapServer, 389);
+                    // ... Bind CN amb l'usuari readonly
                     cn.Bind(LdapCNUser, LdapCNPassword);
 
                     // Buscar entre els usuaris
                     var filtre = $"(uid={username})";
-                    var resutatcerca = cn.Search("ou=People,dc=aliga,dc=cat",
+                    var resutatcerca = cn.Search(LdapUsers,
                                                   LdapConnection.SCOPE_SUB,
                                                   filtre,
                                                   null,
